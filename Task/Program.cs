bc54da8ff73eede27dfd48b45ff2dc91da59ca8d
SergyVR <rsv1177@gmail.com>
2022-12-02 02:39:55 +0300

    4 commit

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayIn = new string[9] {"мир", "дружба", "жвачка", "GB", "8", "800", "700", "68", "41"};
+string[] arrayOut = new string[arrayIn.Length];
+
+
+    int count = 0;
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+    if(arrayIn[i].Length <= 3)
+        {
+        arrayOut[count] = arrayIn[i];
+        count++;
+        }
+    }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arrayOut);
